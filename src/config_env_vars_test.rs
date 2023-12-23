eca8b4c5e19b3308f78f038927e1b5aa30aa5640
mfirhas <mfathirirhas@gmail.com>
2023-12-23 14:38:52 +0700

    Add failed test test_env_vars_success

--- src/config_env_vars_test.rs
+++ src/config_env_vars_test.rs
@@ -3,9 +3,44 @@
 use serde::Deserialize;
 
 use crate::config::*;
+use std::env;
 
 #[test]
-fn test_env_vars_success() {}
+fn test_env_vars_success() {
+    // setup
+    env::set_var("ENV_STRING", "anu");
+    env::set_var("ENV_INT", "123");
+    env::set_var("ENV_FLOAT", "123.0");
+    env::set_var("ENV_BOOL", "true");
+    env::set_var("ENV_ARR", "123,234,345,456");
+
+    // type
+    #[derive(Debug, Deserialize)]
+    struct Cfg {
+        #[serde(alias = "ENV_STRING")]
+        string: String,
+        #[serde(alias = "ENV_INT")]
+        int: i64,
+        #[serde(alias = "ENV_FLOAT")]
+        float: f64,
+        #[serde(alias = "ENV_BOOL")]
+        boolean: bool,
+        #[serde(alias = "ENV_ARR")]
+        arr: Vec<i32>,
+    }
+
+    // run
+    let cfg = Config::new().build::<Cfg>();
+
+    // assert
+    assert!(&cfg.is_ok());
+    let cfg = cfg.unwrap();
+    assert_eq!(cfg.string, "anu");
+    assert_eq!(cfg.boolean, false);
+    assert_eq!(cfg.int, 123);
+    assert_eq!(cfg.float, 123.0);
+    assert_eq!(cfg.arr, vec![123, 234, 345, 456]);
+}
 
 #[test]
 fn test_env_vars_missing_field_failed() {}
