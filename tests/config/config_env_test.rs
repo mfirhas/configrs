4077ab979579ec4fe70c3366eced2bb0165dd5b8
mfirhas <mfathirirhas@gmail.com>
2024-01-05 20:25:39 +0700

    Pass test_env_custom_default_success

--- tests/config/config_env_test.rs
+++ tests/config/config_env_test.rs
@@ -229,7 +229,7 @@ fn test_env_default_success() {
 // load with custom default value for non-existing env in env file
 #[test]
 fn test_env_custom_default_success() {
-    let file_path = "../tests/data/env/test_missing_field.env";
+    let file_path = "./tests/data/env/test_missing_field.env";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         #[serde(alias = "ENV_STRING")]
@@ -240,11 +240,9 @@ fn test_env_custom_default_success() {
         pub float: f64,
         #[serde(alias = "ENV_BOOLEAN", default = "default_boolean")]
         pub boolean: bool,
-        #[serde(alias = "ENV_ARR")]
-        pub arr: Vec<String>,
-        #[serde(alias = "SUB_ENV")]
+        #[serde(flatten)]
         pub sub_env: SubEnv,
-        #[serde(alias = "SUB_ENV_2")]
+        #[serde(flatten)]
         pub sub_env_2: SubEnv2,
     }
     const fn default_boolean() -> bool {
@@ -261,9 +259,7 @@ fn test_env_custom_default_success() {
         pub sub_env_float: f64,
         #[serde(alias = "SUB_ENV_BOOLEAN")]
         pub sub_env_boolean: bool,
-        #[serde(alias = "SUB_ENV_ARR")]
-        pub sub_env_arr: Vec<String>,
-        #[serde(alias = "SUB_SUB_ENV")]
+        #[serde(flatten)]
         pub sub_sub_env: SubSubEnv,
     }
 
@@ -277,8 +273,6 @@ fn test_env_custom_default_success() {
         pub sub_sub_env_float: f64,
         #[serde(alias = "SUB_SUB_ENV_BOOLEAN")]
         pub sub_sub_env_boolean: bool,
-        #[serde(alias = "SUB_SUB_ENV_ARR")]
-        pub sub_sub_env_arr: Vec<String>,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -291,42 +285,28 @@ fn test_env_custom_default_success() {
         pub sub_env_2_float: f64,
         #[serde(alias = "SUB_ENV_2_BOOLEAN")]
         pub sub_env_2_boolean: bool,
-        #[serde(alias = "SUB_ENV_2_ARR")]
-        pub sub_env_2_arr: Vec<String>,
     }
 
     let cfg = Config::new().with_env(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_ok());
     let cfg = cfg.unwrap();
     assert_eq!(cfg.string, "string");
     assert_eq!(cfg.integer, 123);
     assert_eq!(cfg.float, 123.0);
     assert_eq!(cfg.boolean, true);
-    assert_eq!(cfg.arr, vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]);
     assert_eq!(cfg.sub_env.sub_env_string, "string");
     assert_eq!(cfg.sub_env.sub_env_integer, 123);
     assert_eq!(cfg.sub_env.sub_env_float, 123.0);
     assert_eq!(cfg.sub_env.sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
     assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_string, "string");
     assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_integer, 123);
     assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_float, 123.0);
     assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_sub_env.sub_sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
     assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
     assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
     assert_eq!(cfg.sub_env_2.sub_env_2_float, 123.0);
     assert_eq!(cfg.sub_env_2.sub_env_2_boolean, true);
-    assert_eq!(
-        cfg.sub_env_2.sub_env_2_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
 }
 
 // load with different letter case between env file and structs's fields
