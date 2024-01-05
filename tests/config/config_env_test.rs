863b6a46a6646c30b0d69145f3329558836d1341
mfirhas <mfathirirhas@gmail.com>
2024-01-05 21:14:50 +0700

    Pass config_env_test

--- tests/config/config_env_test.rs
+++ tests/config/config_env_test.rs
@@ -312,7 +312,7 @@ fn test_env_custom_default_success() {
 // load with different letter case between env file and structs's fields
 #[test]
 fn test_env_field_case_sensitive_failed() {
-    let file_path = "../tests/data/env/test.env";
+    let file_path = "./tests/data/env/test.env";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         // #[serde(alias = "ENV_STRING")]
@@ -376,13 +376,14 @@ fn test_env_field_case_sensitive_failed() {
     }
 
     let cfg = Config::new().with_env(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_err());
 }
 
 // load with different letter case between env file and structs's serde alias
 #[test]
 fn test_env_alias_case_sensitive_failed() {
-    let file_path = "../tests/data/env/test.env";
+    let file_path = "./tests/data/env/test.env";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         #[serde(alias = "ENV_STRiNG")]
@@ -446,219 +447,6 @@ fn test_env_alias_case_sensitive_failed() {
     }
 
     let cfg = Config::new().with_env(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_err());
-}
-
-// Load config from env vars then overwritten by .env file with overwrite, should success
-#[test]
-fn test_with_overwrite() {
-    std::env::set_var("SUB_ENV_FLOAT", "234.3450"); // will be overwritten by 123.0 from test.env
-
-    let file_path = "../tests/data/env/test.env";
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Env {
-        #[serde(alias = "ENV_STRING")]
-        pub string: String,
-        #[serde(alias = "ENV_INTEGER")]
-        pub integer: i64,
-        #[serde(alias = "ENV_FLOAT")]
-        pub float: f64,
-        #[serde(alias = "ENV_BOOLEAN")]
-        pub boolean: bool,
-        #[serde(alias = "ENV_ARR")]
-        pub arr: Vec<String>,
-        #[serde(alias = "SUB_ENV")]
-        pub sub_env: SubEnv,
-        #[serde(alias = "SUB_ENV_2")]
-        pub sub_env_2: SubEnv2,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv {
-        #[serde(alias = "SUB_ENV_STRING")]
-        pub sub_env_string: String,
-        #[serde(alias = "SUB_ENV_INTEGER")]
-        pub sub_env_integer: i64,
-        #[serde(alias = "SUB_ENV_FLOAT")]
-        pub sub_env_float: f64,
-        #[serde(alias = "SUB_ENV_BOOLEAN")]
-        pub sub_env_boolean: bool,
-        #[serde(alias = "SUB_ENV_ARR")]
-        pub sub_env_arr: Vec<String>,
-        #[serde(alias = "SUB_SUB_ENV")]
-        pub sub_sub_env: SubSubEnv,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubSubEnv {
-        #[serde(alias = "SUB_SUB_ENV_STRING")]
-        pub sub_sub_env_string: String,
-        #[serde(alias = "SUB_SUB_ENV_INTEGER")]
-        pub sub_sub_env_integer: i64,
-        #[serde(alias = "SUB_SUB_ENV_FLOAT")]
-        pub sub_sub_env_float: f64,
-        #[serde(alias = "SUB_SUB_ENV_BOOLEAN")]
-        pub sub_sub_env_boolean: bool,
-        #[serde(alias = "SUB_SUB_ENV_ARR")]
-        pub sub_sub_env_arr: Vec<String>,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv2 {
-        #[serde(alias = "SUB_ENV_2_STRING")]
-        pub sub_env_2_string: String,
-        #[serde(alias = "SUB_ENV_2_INTEGER")]
-        pub sub_env_2_integer: i64,
-        #[serde(alias = "SUB_ENV_2_FLOAT")]
-        pub sub_env_2_float: f64,
-        #[serde(alias = "SUB_ENV_2_BOOLEAN")]
-        pub sub_env_2_boolean: bool,
-        #[serde(alias = "SUB_ENV_2_ARR")]
-        pub sub_env_2_arr: Vec<String>,
-    }
-
-    let cfg = Config::new()
-        .with_overwrite()
-        .with_env(file_path)
-        .build::<Env>();
-    assert!(cfg.is_ok());
-    let cfg = cfg.unwrap();
-    assert_eq!(cfg.string, "string");
-    assert_eq!(cfg.integer, 123);
-    assert_eq!(cfg.float, 123.0);
-    assert_eq!(cfg.boolean, true);
-    assert_eq!(cfg.arr, vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]);
-    assert_eq!(cfg.sub_env.sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_sub_env.sub_sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
-    assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
-    assert_eq!(cfg.sub_env_2.sub_env_2_float, 123.0);
-    assert_eq!(cfg.sub_env_2.sub_env_2_boolean, true);
-    assert_eq!(
-        cfg.sub_env_2.sub_env_2_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-
-    std::env::remove_var("SUB_ENV_FLOAT");
-}
-
-// Load config from env vars then overwritten by .env file without overwrite, should failed
-#[test]
-fn test_without_overwrite() {
-    std::env::set_var("SUB_ENV_FLOAT", "234.3450");
-
-    let file_path = "../tests/data/env/test.env";
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Env {
-        #[serde(alias = "ENV_STRING")]
-        pub string: String,
-        #[serde(alias = "ENV_INTEGER")]
-        pub integer: i64,
-        #[serde(alias = "ENV_FLOAT")]
-        pub float: f64,
-        #[serde(alias = "ENV_BOOLEAN")]
-        pub boolean: bool,
-        #[serde(alias = "ENV_ARR")]
-        pub arr: Vec<String>,
-        #[serde(alias = "SUB_ENV")]
-        pub sub_env: SubEnv,
-        #[serde(alias = "SUB_ENV_2")]
-        pub sub_env_2: SubEnv2,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv {
-        #[serde(alias = "SUB_ENV_STRING")]
-        pub sub_env_string: String,
-        #[serde(alias = "SUB_ENV_INTEGER")]
-        pub sub_env_integer: i64,
-        #[serde(alias = "SUB_ENV_FLOAT")]
-        pub sub_env_float: f64,
-        #[serde(alias = "SUB_ENV_BOOLEAN")]
-        pub sub_env_boolean: bool,
-        #[serde(alias = "SUB_ENV_ARR")]
-        pub sub_env_arr: Vec<String>,
-        #[serde(alias = "SUB_SUB_ENV")]
-        pub sub_sub_env: SubSubEnv,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubSubEnv {
-        #[serde(alias = "SUB_SUB_ENV_STRING")]
-        pub sub_sub_env_string: String,
-        #[serde(alias = "SUB_SUB_ENV_INTEGER")]
-        pub sub_sub_env_integer: i64,
-        #[serde(alias = "SUB_SUB_ENV_FLOAT")]
-        pub sub_sub_env_float: f64,
-        #[serde(alias = "SUB_SUB_ENV_BOOLEAN")]
-        pub sub_sub_env_boolean: bool,
-        #[serde(alias = "SUB_SUB_ENV_ARR")]
-        pub sub_sub_env_arr: Vec<String>,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv2 {
-        #[serde(alias = "SUB_ENV_2_STRING")]
-        pub sub_env_2_string: String,
-        #[serde(alias = "SUB_ENV_2_INTEGER")]
-        pub sub_env_2_integer: i64,
-        #[serde(alias = "SUB_ENV_2_FLOAT")]
-        pub sub_env_2_float: f64,
-        #[serde(alias = "SUB_ENV_2_BOOLEAN")]
-        pub sub_env_2_boolean: bool,
-        #[serde(alias = "SUB_ENV_2_ARR")]
-        pub sub_env_2_arr: Vec<String>,
-    }
-
-    let cfg = Config::new()
-        // .with_overwrite()
-        .with_env(file_path)
-        .build::<Env>();
-    assert!(cfg.is_ok());
-    let cfg = cfg.unwrap();
-    assert_eq!(cfg.string, "string");
-    assert_eq!(cfg.integer, 123);
-    assert_eq!(cfg.float, 123.0);
-    assert_eq!(cfg.boolean, true);
-    assert_eq!(cfg.arr, vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]);
-    assert_eq!(cfg.sub_env.sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_sub_env.sub_sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
-    assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
-    assert_eq!(cfg.sub_env_2.sub_env_2_float, 123.0);
-    assert_eq!(cfg.sub_env_2.sub_env_2_boolean, true);
-    assert_eq!(
-        cfg.sub_env_2.sub_env_2_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-
-    std::env::remove_var("SUB_ENV_FLOAT");
 }
