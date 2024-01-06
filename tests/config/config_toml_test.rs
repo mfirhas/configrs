6250c2aa577edc0b04a8c03c57b001b7538708ee
mfirhas <mfathirirhas@gmail.com>
2024-01-06 11:56:24 +0700

    Pass toml tests

--- tests/config/config_toml_test.rs
+++ tests/config/config_toml_test.rs
@@ -108,7 +108,7 @@ fn test_toml_success() {
 // there is missing env key in env file
 #[test]
 fn test_toml_missing_key() {
-    let file_path = "../tests/data/toml/test_missing_field.toml";
+    let file_path = "./tests/data/toml/test_missing_field.toml";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         #[serde(alias = "env_string")]
@@ -171,7 +171,8 @@ fn test_toml_missing_key() {
         pub sub_env_2_arr: Vec<String>,
     }
 
-    let cfg = Config::new().with_json(file_path).build::<Env>();
+    let cfg = Config::new().with_toml(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_err());
     let cfg = cfg.unwrap_err();
 }
@@ -179,7 +180,7 @@ fn test_toml_missing_key() {
 // load with default(trait) value for non-existing env in env file and success
 #[test]
 fn test_toml_default_success() {
-    let file_path = "../tests/data/toml/test_missing_field.toml";
+    let file_path = "./tests/data/toml/test_missing_field.toml";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         #[serde(alias = "env_string")]
@@ -188,7 +189,7 @@ fn test_toml_default_success() {
         pub integer: i64,
         #[serde(alias = "env_float")]
         pub float: f64,
-        #[serde(alias = "env_boolean", default)]
+        #[serde(alias = "env_boolean")]
         pub boolean: bool,
         #[serde(alias = "env_arr")]
         pub arr: Vec<String>,
@@ -224,7 +225,7 @@ fn test_toml_default_success() {
         pub sub_sub_env_float: f64,
         #[serde(alias = "sub_sub_env_boolean")]
         pub sub_sub_env_boolean: bool,
-        #[serde(alias = "sub_sub_env_arr")]
+        #[serde(alias = "sub_sub_env_arr", default)]
         pub sub_sub_env_arr: Vec<String>,
     }
 
@@ -242,110 +243,8 @@ fn test_toml_default_success() {
         pub sub_env_2_arr: Vec<String>,
     }
 
-    let cfg = Config::new().with_json(file_path).build::<Env>();
-    assert!(cfg.is_ok());
-    let cfg = cfg.unwrap();
-    assert_eq!(cfg.string, "string");
-    assert_eq!(cfg.integer, 123);
-    assert_eq!(cfg.float, 123.0);
-    assert_eq!(cfg.boolean, false);
-    assert_eq!(cfg.arr, vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]);
-    assert_eq!(cfg.sub_env.sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_string, "string");
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_integer, 123);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_float, 123.0);
-    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
-    assert_eq!(
-        cfg.sub_env.sub_sub_env.sub_sub_env_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-    assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
-    assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
-    assert_eq!(cfg.sub_env_2.sub_env_2_float, 123.0);
-    assert_eq!(cfg.sub_env_2.sub_env_2_boolean, true);
-    assert_eq!(
-        cfg.sub_env_2.sub_env_2_arr,
-        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
-    );
-}
-
-// load with custom default value for non-existing env in env file
-#[test]
-fn test_toml_custom_default_success() {
-    let file_path = "../tests/data/toml/test_missing_field.toml";
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct Env {
-        #[serde(alias = "env_string")]
-        pub string: String,
-        #[serde(alias = "env_integer")]
-        pub integer: i64,
-        #[serde(alias = "env_float")]
-        pub float: f64,
-        #[serde(alias = "env_boolean", default = "default_boolean")]
-        pub boolean: bool,
-        #[serde(alias = "env_arr")]
-        pub arr: Vec<String>,
-        #[serde(alias = "sub_env")]
-        pub sub_env: SubEnv,
-        #[serde(alias = "sub_env_2")]
-        pub sub_env_2: SubEnv2,
-    }
-    const fn default_boolean() -> bool {
-        true
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv {
-        #[serde(alias = "sub_env_string")]
-        pub sub_env_string: String,
-        #[serde(alias = "sub_env_integer")]
-        pub sub_env_integer: i64,
-        #[serde(alias = "sub_env_float")]
-        pub sub_env_float: f64,
-        #[serde(alias = "sub_env_boolean")]
-        pub sub_env_boolean: bool,
-        #[serde(alias = "sub_env_arr")]
-        pub sub_env_arr: Vec<String>,
-        #[serde(alias = "sub_sub_env")]
-        pub sub_sub_env: SubSubEnv,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubSubEnv {
-        #[serde(alias = "sub_sub_env_string")]
-        pub sub_sub_env_string: String,
-        #[serde(alias = "sub_sub_env_integer")]
-        pub sub_sub_env_integer: i64,
-        #[serde(alias = "sub_sub_env_float")]
-        pub sub_sub_env_float: f64,
-        #[serde(alias = "sub_sub_env_boolean")]
-        pub sub_sub_env_boolean: bool,
-        #[serde(alias = "sub_sub_env_arr")]
-        pub sub_sub_env_arr: Vec<String>,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    pub struct SubEnv2 {
-        #[serde(alias = "sub_env_2_string")]
-        pub sub_env_2_string: String,
-        #[serde(alias = "sub_env_2_integer")]
-        pub sub_env_2_integer: i64,
-        #[serde(alias = "sub_env_2_float")]
-        pub sub_env_2_float: f64,
-        #[serde(alias = "sub_env_2_boolean")]
-        pub sub_env_2_boolean: bool,
-        #[serde(alias = "sub_env_2_arr")]
-        pub sub_env_2_arr: Vec<String>,
-    }
-
-    let cfg = Config::new().with_json(file_path).build::<Env>();
+    let cfg = Config::new().with_toml(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_ok());
     let cfg = cfg.unwrap();
     assert_eq!(cfg.string, "string");
@@ -367,7 +266,111 @@ fn test_toml_custom_default_success() {
     assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
     assert_eq!(
         cfg.sub_env.sub_sub_env.sub_sub_env_arr,
+        Vec::<String>::new()
+    );
+    assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
+    assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
+    assert_eq!(cfg.sub_env_2.sub_env_2_float, 123.0);
+    assert_eq!(cfg.sub_env_2.sub_env_2_boolean, true);
+    assert_eq!(
+        cfg.sub_env_2.sub_env_2_arr,
         vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
+    );
+}
+
+// load with custom default value for non-existing env in env file
+#[test]
+fn test_toml_custom_default_success() {
+    let file_path = "./tests/data/toml/test_missing_field.toml";
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Env {
+        #[serde(alias = "env_string")]
+        pub string: String,
+        #[serde(alias = "env_integer")]
+        pub integer: i64,
+        #[serde(alias = "env_float")]
+        pub float: f64,
+        #[serde(alias = "env_boolean")]
+        pub boolean: bool,
+        #[serde(alias = "env_arr")]
+        pub arr: Vec<String>,
+        #[serde(alias = "sub_env")]
+        pub sub_env: SubEnv,
+        #[serde(alias = "sub_env_2")]
+        pub sub_env_2: SubEnv2,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct SubEnv {
+        #[serde(alias = "sub_env_string")]
+        pub sub_env_string: String,
+        #[serde(alias = "sub_env_integer")]
+        pub sub_env_integer: i64,
+        #[serde(alias = "sub_env_float")]
+        pub sub_env_float: f64,
+        #[serde(alias = "sub_env_boolean")]
+        pub sub_env_boolean: bool,
+        #[serde(alias = "sub_env_arr")]
+        pub sub_env_arr: Vec<String>,
+        #[serde(alias = "sub_sub_env")]
+        pub sub_sub_env: SubSubEnv,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct SubSubEnv {
+        #[serde(alias = "sub_sub_env_string")]
+        pub sub_sub_env_string: String,
+        #[serde(alias = "sub_sub_env_integer")]
+        pub sub_sub_env_integer: i64,
+        #[serde(alias = "sub_sub_env_float")]
+        pub sub_sub_env_float: f64,
+        #[serde(alias = "sub_sub_env_boolean")]
+        pub sub_sub_env_boolean: bool,
+        #[serde(alias = "sub_sub_env_arr", default = "default_arr")]
+        pub sub_sub_env_arr: Vec<String>,
+    }
+    fn default_arr() -> Vec<String> {
+        vec!["anu".to_string(), "nganu".to_string()]
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct SubEnv2 {
+        #[serde(alias = "sub_env_2_string")]
+        pub sub_env_2_string: String,
+        #[serde(alias = "sub_env_2_integer")]
+        pub sub_env_2_integer: i64,
+        #[serde(alias = "sub_env_2_float")]
+        pub sub_env_2_float: f64,
+        #[serde(alias = "sub_env_2_boolean")]
+        pub sub_env_2_boolean: bool,
+        #[serde(alias = "sub_env_2_arr")]
+        pub sub_env_2_arr: Vec<String>,
+    }
+
+    let cfg = Config::new().with_toml(file_path).build::<Env>();
+    dbg!(&cfg);
+    assert!(cfg.is_ok());
+    let cfg = cfg.unwrap();
+    assert_eq!(cfg.string, "string");
+    assert_eq!(cfg.integer, 123);
+    assert_eq!(cfg.float, 123.0);
+    assert_eq!(cfg.boolean, true);
+    assert_eq!(cfg.arr, vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]);
+    assert_eq!(cfg.sub_env.sub_env_string, "string");
+    assert_eq!(cfg.sub_env.sub_env_integer, 123);
+    assert_eq!(cfg.sub_env.sub_env_float, 123.0);
+    assert_eq!(cfg.sub_env.sub_env_boolean, true);
+    assert_eq!(
+        cfg.sub_env.sub_env_arr,
+        vec!["anu", "nganu", "lskmdf", "lwkef", "lkemrg"]
+    );
+    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_string, "string");
+    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_integer, 123);
+    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_float, 123.0);
+    assert_eq!(cfg.sub_env.sub_sub_env.sub_sub_env_boolean, true);
+    assert_eq!(
+        cfg.sub_env.sub_sub_env.sub_sub_env_arr,
+        vec!["anu", "nganu"]
     );
     assert_eq!(cfg.sub_env_2.sub_env_2_string, "string");
     assert_eq!(cfg.sub_env_2.sub_env_2_integer, 123);
@@ -382,7 +385,7 @@ fn test_toml_custom_default_success() {
 // load with different letter case between env file and structs's fields
 #[test]
 fn test_toml_field_case_sensitive_failed() {
-    let file_path = "../tests/data/toml/test.toml";
+    let file_path = "./tests/data/toml/test.toml";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         // #[serde(alias = "env_string")]
@@ -445,14 +448,15 @@ fn test_toml_field_case_sensitive_failed() {
         pub sub_env_2_arr: Vec<String>,
     }
 
-    let cfg = Config::new().with_json(file_path).build::<Env>();
+    let cfg = Config::new().with_toml(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_err());
 }
 
 // load with different letter case between env file and structs's serde alias
 #[test]
 fn test_toml_alias_case_sensitive_failed() {
-    let file_path = "../tests/data/toml/test.toml";
+    let file_path = "./tests/data/toml/test.toml";
     #[derive(Debug, Serialize, Deserialize)]
     pub struct Env {
         #[serde(alias = "env_strinG")]
@@ -515,7 +519,8 @@ fn test_toml_alias_case_sensitive_failed() {
         pub sub_env_2_arr: Vec<String>,
     }
 
-    let cfg = Config::new().with_json(file_path).build::<Env>();
+    let cfg = Config::new().with_toml(file_path).build::<Env>();
+    dbg!(&cfg);
     assert!(cfg.is_err());
 }
 
